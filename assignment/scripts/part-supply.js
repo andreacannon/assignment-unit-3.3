7ce3ece639baf1a329377f42be82fee2b75d0812
Andrea Cannon <cannonac1049@gmail.com>
2024-03-17 21:19:28 -0500

    corrected stretch goals

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -95,6 +95,14 @@ console.log('Supply:', supplyChanges);
 //    "left over," and the value of 'boxesFilled' should reflect
 //    how many boxes were filled.
 console.log('9. Filling boxes with a "while" loop');
+let parts = 572;
+let boxesFilled = 0;
+while (parts >= 7){
+  parts -= 7;
+  boxesFilled += 1;
+}
+console.log('parts:',parts);
+console.log('filled',boxesFilled,'boxes');
 
 
 
